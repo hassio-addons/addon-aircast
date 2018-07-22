7fe2391b31f961e11a90dd985547566e59f7f33d
Franck Nijhof <frenck@addons.community>
2018-07-22 16:35:01 +0200

    :sparkles: Adds support for tweaking AirCast using its configuration file

--- aircast/rootfs/etc/cont-init.d/50-aircast.sh
+++ aircast/rootfs/etc/cont-init.d/50-aircast.sh
@@ -8,8 +8,13 @@ source /usr/lib/hassio-addons/base.sh
 
 declare latency
 
-latency=$(hass.config.get 'latency_rtp')
+# Create a configuration file, if it does not exist yet
+if ! hass.file_exists '/config/aircast.xml'; then
+    cp /etc/aircast.xml /config/aircast.xml
+fi
 
+# Warn if latency is below 500ms
+latency=$(hass.config.get 'latency_rtp')
 if [[ "${latency}" -lt 500 && "${latency}" -ne 0 ]]; then
     hass.log.warning \
         'Setting the RTP latency of AirPlay audio below 500ms is not recommended!'
