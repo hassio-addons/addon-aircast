62acc371d9d527ac2c5f534fcc82b90b49e96f58
Franck Nijhof <frenck@addons.community>
2019-03-20 21:31:48 +0100

    :hammer: Rewrite add-on onto Bashio

--- aircast/rootfs/etc/cont-init.d/50-aircast.sh
+++ aircast/rootfs/etc/cont-init.d/50-aircast.sh
@@ -1,21 +1,18 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: AirCast
 # Checks latency settings before starting the AirCast server
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
-
 declare latency
 
 # Create a configuration file, if it does not exist yet
-if ! hass.file_exists '/config/aircast.xml'; then
+if ! bashio::fs.file_exists '/config/aircast.xml'; then
     cp /etc/aircast.xml /config/aircast.xml
 fi
 
 # Warn if latency is below 500ms
-latency=$(hass.config.get 'latency_rtp')
+latency=$(bashio::config 'latency_rtp')
 if [[ "${latency}" -lt 500 && "${latency}" -ne 0 ]]; then
-    hass.log.warning \
+    bashio::log.warning \
         'Setting the RTP latency of AirPlay audio below 500ms is not recommended!'
 fi
