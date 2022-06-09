3e5d0cd8426fa4e3024c48f3a2b2c02af454dd4e
Franck Nijhof <git@frenck.dev>
2022-06-09 11:57:49 +0200

    ⬆️ Upgrades add-on base image to 6.0.0

--- aircast/rootfs/etc/cont-init.d/aircast.sh
+++ aircast/rootfs/etc/cont-init.d/aircast.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: AirCast
 # Checks latency settings before starting the AirCast server
